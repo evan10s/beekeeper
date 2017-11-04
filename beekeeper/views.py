dd6246dd881d4947802ae391813cab6e7f7336a9
Isaac Weintraub <iweintraub3@gatech.edu>
2017-11-04 12:39:53 -0400

    commit

--- beekeeper/views.py
+++ beekeeper/views.py
@@ -6,6 +6,7 @@ from .models import Treatment
 @csrf_exempt
 def myTestView(request):
     if request.method == "POST":
+        #request.data =
         Treatment.objects.get(bg.__gte__="70")
-        return HttpResponse(json.dumps({'speech': 'Hi there', "displayText": "Hi there!"}),content_type="text/json")
+        return HttpResponse(json.dumps({'data',request.POST,'speech': 'Hi there', "displayText": "Hi there!"}),content_type="text/json")
     return HttpResponseForbidden("403 Forbidden")
