0da6fd6df73fdd172012d685a37fd4758f36d757
Evan Strat <evan10s@users.noreply.github.com>
2017-11-04 11:54:16 -0400

    disable csrf protection django response

--- beekeeper/views.py
+++ beekeeper/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
+@csrf_exempt
 def myTestView(request):
     return HttpResponse(json.dumps({'speech': 'Hi there', "displayText": "Hi there!"}),content_type="text/json")
