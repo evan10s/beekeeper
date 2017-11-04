aae3a6ec761e59504093250b8de30187770cddb9
Evan Strat <evan10s@users.noreply.github.com>
2017-11-04 17:39:47 -0400

    Add timestamp to model

--- beekeeper/models.py
+++ beekeeper/models.py
@@ -19,6 +19,7 @@ class Treatment(models.Model):
     bg = models.IntegerField(verbose_name="BG")
     dose = models.DecimalField(verbose_name="Insulin dose",max_digits=4, decimal_places=2)
     carbs = models.IntegerField()
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return self.meal + " " + str(self.dose)
