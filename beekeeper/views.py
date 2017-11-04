010d3d60baa6d5a7ef03ec4f9d54a30eee66568f
Evan Strat <evan10s@users.noreply.github.com>
2017-11-04 13:06:19 -0400

    debugging fulfillment

--- beekeeper/views.py
+++ beekeeper/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from .models import Treatment
+#from .models import Treatment
 
 @csrf_exempt
 def myTestView(request):
     if request.method == "POST":
         #request.data =
-        Treatment.objects.get(bg.__gte__="70")
-        return HttpResponse(json.dumps({'data',request.POST,'speech': 'Hi there', "displayText": "Hi there!"}),content_type="text/json")
+        #Treatment.objects.get(bg.__gte__="70")
+        #print(request.POST.get('lang',0))
+        #return HttpResponse(json.dumps({'speech': 'Hi there', "displayText": "Hi there!"}),content_type="text/json")
+        return HttpResponse(json.dumps({'displayText':request.body.result.action, 'speech':"It worked"}))
     return HttpResponseForbidden("403 Forbidden")
